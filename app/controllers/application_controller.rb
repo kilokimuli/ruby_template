24c5c9ed2b01c10d7737a78c7d042f0376be2f91
kiloki <simeonkiloki@gmail.com>
2024-10-22 14:14:27 +0300

    Set up letter opener, generate devise views and add missing dependencies
    configure letter opener gem in development environment
    generate devise views and add additional fields
    add validation to user model
    fix typo in application controller
    add @hotwired/turbo-rails and @hotwired/stimulus to package.json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_contriller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   # skip_before_action :authenticate_user!, only: [:show]
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer(:sign_up, keys: %i[first_name last_name terms_and_conditions])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name terms_and_conditions])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[irst_name last_name])
   end
 end
